80b7beae10d824e1155462d217cc578cbd39b5a7
jackchi <fuching.chi@gmail.com>
2014-10-21 16:32:12 -0700

    Factorial now caching level-specific factorials

--- python/factorial.py
+++ python/factorial.py
@@ -21,13 +21,39 @@ def factorial(number):
 	else:
 		return 1
 
+def allFactorials(number):
+	""" Using a wrapper to initialized the Array """
+	results = [1] if number == 0 else list((0,)*(number+1))
+	doAllFactorials(number, 0, results)
+	return results
+
+def doAllFactorials(number, level, results):
+	"""n!! = [n!, (n-1)!, (n-2)!, ..., 0!] """
+	if(number >= 1):
+		results[level] = number * doAllFactorials(number-1, level+1, results)
+		return results[level]
+	else:
+		results[level] = 1
+		return 1
+
 def main():
 	args = sys.argv[1:]
 
+	AllFacts = False
+	if (args[0] == '--all'):
+		AllFacts = True
+		del args[0]
+	
 	if not args:
-		print 'usage: number1 [number2]'
-	for number in args:
-		print '%s! = %d' % (number, factorial(int(number)))		
+		print 'usage: --all number1 [number2]'
+		sys.exit(1)
+			
+	if AllFacts:
+		for number in args:
+			print '%s! has these levels:\n' + str(allFactorials(int(number)))
+	else:
+		for number in args:
+			print '%s! = %d' % (number, factorial(int(number)))		
 
 if __name__ == '__main__':
 	main()
