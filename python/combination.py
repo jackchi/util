000220276b0a7587670140a266bf2d04c77d6a43
jackchi <fuching.chi@gmail.com>
2014-10-23 23:51:15 -0700

    Combination problem solved with genertors and recursion

---
+++ python/combination.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+# https://github.com/jackchi/util
+# Copyright 2014 Jack Chi
+# 
+# Python String Practice
+# Using Recursion and Generators to find all combinations of
+# a string
+import sys
+
+def combination(string):
+	""" Generator to yield the current combination """
+	if not string:
+		yield ''
+	for i, d in enumerate(string):
+		for comb in combination(string[i+1:]):
+			yield d + comb
+	
+
+def main():
+	args = sys.argv[1:]
+
+	if not args:
+		print 'usage: string'
+
+	combinations = []
+	for c in combination(args[0]):
+		combinations.append(c)
+
+	print combinations	
+
+if __name__ == '__main__':
+	main()	
