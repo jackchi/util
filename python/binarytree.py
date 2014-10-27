a70f879c29a60816ca2a06cc12649c83682f1dfb
jackchi <fuching.chi@gmail.com>
2014-10-27 12:29:57 -0700

    Binary Tree Class Representation and Self Test

---
+++ python/binarytree.py
@@ -0,0 +1,108 @@
+#!/usr/bin/python
+# https://github.com/jackchi/util
+# Copyright 2014 Jack Chi
+
+# Binary Tree Nodes Representation
+import Queue
+import unittest
+
+class BinaryTreeTest(unittest.TestCase):
+
+	@classmethod
+	def setUpClass(self):
+		self.tree = BinaryTree('root')
+
+	@classmethod	
+	def tearUpClass(self):
+		del self.tree
+
+	def test_1(self):
+		self.assertIsInstance(
+			self.tree,
+			BinaryTree
+		)
+
+		self.assertIsNone(self.tree.getLeftChild())
+
+		self.assertIsNone(self.tree.getRightChild())
+
+	def test_2(self):
+		self.tree.insertLeft('left')
+		self.tree.insertRight('right')
+
+		self.assertEqual(
+			self.tree.getLeftChild().key, 'left')
+		self.assertEqual(
+			self.tree.getRightChild().key, 'right')
+
+	def test_3(self):
+		self.tree.getLeftChild().insertLeft('leftmost')
+		self.tree.getRightChild().insertRight('rightmost')
+
+		self.assertIsNone(self.tree.BFSearch('nothing'))
+		self.assertEqual(
+			self.tree.BFSearch('leftmost').key, 
+			'leftmost')
+		self.assertEqual(
+			self.tree.BFSearch('rightmost').key, 
+			'rightmost')	
+
+	def test_4(self):
+		self.tree.setRootVal('newRoot')
+
+		self.assertEqual(
+			self.tree.getRootVal(), 'newRoot'
+			)	
+			
+
+class BinaryTree:
+	def __init__(self, rootObj):
+		self.key = rootObj
+		self.leftChild = None
+		self.rightChild = None
+
+	def insertLeft(self, newNode):
+		if not self.leftChild:
+			self.leftChild = BinaryTree(newNode)
+		else:
+			t = BinaryTree(newNode)
+			t.leftChild = self.leftChild
+			self.leftChild = t
+
+	def insertRight(self, newNode):
+		if not self.rightChild:
+			self.rightChild = BinaryTree(newNode)
+		else:
+			t = BinaryTree(newNode)
+			t.rightChild = self.rightChild
+			self.rightChild = t
+
+	def getLeftChild(self):
+		return self.leftChild		
+
+	def getRightChild(self):
+		return self.rightChild		
+
+	def setRootVal(self, val):
+		self.key = val
+
+	def getRootVal(self):
+		return self.key
+
+	def BFSearch(self, target):
+		queue = Queue.Queue()
+		queue.put(self)
+		
+		while not queue.empty():
+			r = queue.get()
+			if (r.key == target):
+				return r
+			if r.leftChild != None:
+				queue.put(r.leftChild)
+			if r.rightChild != None:
+				queue.put(r.rightChild)	
+		return None
+
+if __name__ == '__main__':
+    unittest.main()
+				
